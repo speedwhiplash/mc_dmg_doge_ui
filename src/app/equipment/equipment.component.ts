d8e221f7ad9fef44396ff85439fb56e06073169a
Speed Whiplash <speedwhiplash@gmail.com>
2020-04-30 16:36:36 -0400

    playing with logo

--- src/app/equipment/equipment.component.ts
+++ src/app/equipment/equipment.component.ts
@@ -11,10 +11,17 @@ export class EquipmentComponent implements OnInit {
   equipment: AllEquipment;
 
   constructor(private httpClient: HttpClient) {
-    httpClient.get('/api/equipment').subscribe((equipment: AllEquipment) => this.equipment = equipment)
+    this.refresh();
   }
 
   ngOnInit(): void {
   }
 
+  refresh() {
+    this.httpClient.get('/api/equipment').subscribe((equipment: AllEquipment) => this.equipment = equipment);
+  }
+
+  update() {
+    this.httpClient.get('/api/update-stats').subscribe()
+  }
 }
